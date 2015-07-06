a8366dfb88f9116acd420c74338a249d99fec209
dchen71 <dchen12812@gmail.com>
2015-07-06 14:11:38 -0700

    added buttons for ajax layout at bottom

--- Scripts/dom.js
+++ Scripts/dom.js
@@ -1,10 +1,26 @@
-$('#content').append('
-	<img src="Images/fresh_food.jpg">
-    <h1>Mystery Spot A</h1>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-      quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-      consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-      cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-      proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>')
+$('#container').append('<img src="Images/fresh_food.jpg">' + 
+    '<h1>Mystery Spot A</h1>' + 
+    '<p>Automatically the bass</p>');
 
+$('#container').append('' +
+		'<button id="home">Home</button>' +
+		'<button id="menu">Menu</button>' +
+		'<button id="contact">Contact</button>');
+
+$('#home').click(function(){
+	$('#content').empty();
+	$('#content').append('<p>Home of the tasties</p>');
+});
+
+$('#menu').click(function(){
+	$('#content').empty();
+	$('#content').append('<p>Catfish $1.00</p>');
+	$('#content').append('<p>Dogfish $13.37</p>');
+});
+
+$('#contact').click(function(){
+	$('#content').empty();
+	$('#content').append('<p>In the bathroom</p>');
+	$('#content').append('<p>Some town, some country, 12345</p>');
+	$('#content').append('<p>123-456-789</p>');
+});
